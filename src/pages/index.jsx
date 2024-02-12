6c2de9ed25e2816f0f4f78204eeb6648c5f1dee0
sanil011 <sanilp191@gmail.com>
2024-02-12 14:58:54 +0530

    remove links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,14 +4,14 @@ import Tictactoe1 from './tictactoe'
 const Index = () => {
   return (
     <div>
-      <h1 className='text-center text-3xl text-black opacity-80 mt-6'>Crafts</h1>
-      <ul className='mx-12 mt-12'>
+      <h1 className='text-center text-3xl text-black opacity-80 mt-6'>Tic Tac Toe</h1>
+      {/* <ul className='mx-12 mt-12'>
         <li>
           <p className=''><Link to={'/tictactoe'}>TicTacToe </Link></p>
         </li>
-      </ul>
+      </ul> */}
 
-      {/* <Tictactoe1/> */}
+      <Tictactoe1/>
     </div>
   )
 }
