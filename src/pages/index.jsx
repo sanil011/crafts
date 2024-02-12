13f42b20719fcadb87248931e8bb03e162b7627f
sanil011 <sanilp191@gmail.com>
2024-02-12 14:49:25 +0530

    home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Tictactoe1 from './tictactoe'
 const Index = () => {
   return (
     <div>
-      <h1 className='text-center text-3xl text-black opacity-80 mt-6'>Crafts</h1>
+      {/* <h1 className='text-center text-3xl text-black opacity-80 mt-6'>Crafts</h1>
       
-      <p className='text-center'><Link to={'/tictactoe'}>TicTacToe </Link></p>
-     
+      <p className='text-center'><Link to={'/tictactoe'}>TicTacToe </Link></p> */}
+     <Tictactoe1/>
     </div>
   )
 }
